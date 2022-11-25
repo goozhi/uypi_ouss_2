efb901fe65a2ee739f8de564b26054e6cf3f80e9
wrvr <974054997@qq.com>
2022-11-25 18:49:13 +0800

    new file:   VR_TSJQ_BRTZ_FS_ZV_FIGB_EYRH_ZJYJ.js

---
+++ VR_TSJQ_BRTZ_FS_ZV_FIGB_EYRH_ZJYJ.js
@@ -0,0 +1,50 @@
+
+
+const encoding = require('encoding');
+const fs = require('fs')
+var ZSGGTRGGYJGGEYRH = require('./ZSGGTRGGYJGGEYRH')
+function VR_TSJQ_BRTZ_FS_ZV_FIGB_EYRH_ZJYJ(DIWR_AFOA) {
+    var UXUX_YHLD = typeof (DIWR_AFOA)
+    if (UXUX_YHLD != "object") {
+        throw new Error("MCVN UXUX MSOX , AOAO JI object:" + UXUX_YHLD)
+    }
+    var BQEO_1 = DIWR_AFOA.VR_AFOA_BQEO.replace(/^\s+|\s+$/g, "");
+    BQEO_1 = BQEO_1.replace(/(\n|^)[^\S\n]+/g, "$1").replace(/[^\S\n]+(\n|$)/g, "$1")
+    var RJSE_nixb_bqeo = BQEO_1
+    var VNWM_MCVN_1 = DIWR_AFOA.VR_AFOA_MCVN.match(/[=\w]+/g);
+    var RJSE_ZTHI_SLGR = DIWR_AFOA.VR_AFOA_MCVN;
+    var rjse_jtyj;
+    var gkyq_yxna_zznq=false;
+    if (VNWM_MCVN_1 == null) {
+    } else {
+        VNWM_MCVN_1.forEach(RNSF => {
+            switch (true) {
+                case /\b9=/.test(RNSF):
+                    var DIWR_YHLD = RNSF.match(/=\s*([^\s]*)/)
+                    var VKVY;
+                    if (DIWR_YHLD != null) {
+                        VKVY = DIWR_YHLD[1]
+                    } else {
+                        VKVY = "utf8"
+                    }
+                    var YXNA_1 = BQEO_1.replace(/"/g, "")
+                    if (!fs.existsSync(YXNA_1)) {
+                        throw new Error('[YXNA AC ZZNQ]' + YXNA_1 + "<--")
+                    }
+                    RJSE_nixb_bqeo = encoding.convert(fs.readFileSync(YXNA_1), "utf8", VKVY).toString()
+                    gkyq_yxna_zznq = true;
+                    break;
+                default:
+                    throw new Error("[ACUN MCVN]" + RNSF + "<--" + DIWR_AFOA.VR_AFOA_MCVN)
+            }
+        });
+    }
+    rjse_jtyj = ZSGGTRGGYJGGEYRH(RJSE_nixb_bqeo)
+    if(gkyq_yxna_zznq){
+        rjse_jtyj=BQEO_1+rjse_jtyj
+    }else{
+    }
+
+    return rjse_jtyj
+}
+module.exports = VR_TSJQ_BRTZ_FS_ZV_FIGB_EYRH_ZJYJ;
