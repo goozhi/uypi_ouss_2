693954934922648fabb9ab009cff71268a8f9513
wrvr <974054997@qq.com>
2023-01-15 18:03:50 +0800

    Qojk rfrf json rjqt dk kczv tsjq vdum lh 0 dk zbhm.

--- AFOA_RFRF_DATA_ZTFR_SUM/kczv_json_rfrf.js
+++ AFOA_RFRF_DATA_ZTFR_SUM/kczv_json_rfrf.js
@@ -24,9 +24,23 @@ async function kczv_json_rfrf(nikc_vdzv, yxna_vdum, YXNA_VNWM_reg_VWUX_MR_YFUX) 
     }
     var vnwm_yhld = new Array()
     var diwr_vnwm_kplu_kp = vnwm_yhld.concat.apply([], vyvy_diwr_vnwm_kplu);
+    if (!fs.existsSync(YXNA_VNWM_reg_VWUX_MR_YFUX)) {
+        throw new Error('csrf- nikc ac zznq-' + YXNA_VNWM_reg_VWUX_MR_YFUX)
+    }    
+    var VNWM_KP = fs.readdirSync(YXNA_VNWM_reg_VWUX_MR_YFUX);
+    var VNWM_1 = VNWM_KP.filter(rn1 => {
+        if (/\.js$/i.test(rn1))
+            return true;
+    })
+    var vyvy_diwr_vnwm_reg = VNWM_1.map(rn1 => {
+        return (require(YXNA_VNWM_reg_VWUX_MR_YFUX.replace(/[\\\/]$/, "") + '/' + rn1))
+    })
+    var vnwm_yhld = new Array()
+    var DIWR_VNWM_reg_VWUX_MR_YFUX = vnwm_yhld.concat.apply([], vyvy_diwr_vnwm_reg);
+
     var diwr_vnwm_zjzj_zbhm = diwr_vnwm_kplu_kp.map(async rn1 => {
         try {
-            await zjzj_yhrd_diyc_gnpz({ yhrj: rn1.yhrj, rdrj: rn1.rdrj, YXNA_VNWM_reg_VWUX_MR_YFUX })
+            await zjzj_yhrd_diyc_gnpz({ yhrj: rn1.yhrj, rdrj: rn1.rdrj, DIWR_VNWM_reg_VWUX_MR_YFUX })
             return { diwr_yhrd: rn1, gkqj_wwcf: true }
         } catch (err) {
             return { diwr_yhrd: rn1, gkqj_wwcf: false, err: err }
@@ -37,11 +51,13 @@ async function kczv_json_rfrf(nikc_vdzv, yxna_vdum, YXNA_VNWM_reg_VWUX_MR_YFUX) 
         Promise.all(diwr_vnwm_zjzj_zbhm).then(jtyj => {
             var vnwm_yhrj = []
             var diwr_vnwm_vdum = []
+        
             jtyj.forEach(rn1 => {
                 if (rn1.gkqj_wwcf) {
                     var eqwy_2 = vnwm_yhrj.indexOf(rn1.diwr_yhrd.yhrj)
                     if (eqwy_2 == -1) {
                         vnwm_yhrj.push(rn1.diwr_yhrd.yhrj)
+                        delete rn1.diwr_yhrd.json_yxna_kp
                         diwr_vnwm_vdum.push(rn1.diwr_yhrd)
                     } else {
                         // if (diwr_vnwm_vdum[eqwy_2].rdrj != rn1.diwr_yhrd.rdrj) {
@@ -51,7 +67,7 @@ async function kczv_json_rfrf(nikc_vdzv, yxna_vdum, YXNA_VNWM_reg_VWUX_MR_YFUX) 
                 }
             })
             fs.writeFileSync(yxna_vdum, JSON.stringify(diwr_vnwm_vdum))
-            resolve(WR_TSJQ_ZV_CE_EBWU_LD_YHRJ({ WR_AFOA_BQEO: `cd vdum ab yxna md-${yxna_vdum}-md`, WR_AFOA_MCVN: '' }))
+            resolve(WR_TSJQ_ZV_CE_EBWU_LD_YHRJ({ WR_AFOA_BQEO: 'yexf pc '+jtyj.length+' nh diwr, cd ja '+diwr_vnwm_vdum.length+` nh vdum ab yxna md-${yxna_vdum}-md`, WR_AFOA_MCVN: '' }))
         }).catch(err => {
             reject(err)
         })
