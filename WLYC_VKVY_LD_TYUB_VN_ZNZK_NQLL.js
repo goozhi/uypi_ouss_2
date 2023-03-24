d41c7ae550e5e60638eb020be2dd4af2e21e6cdc
wrvr <974054997@qq.com>
2023-03-24 11:24:14 +0800

    Sdvu bwcr vy ldrg.

--- WLYC_VKVY_LD_TYUB_VN_ZNZK_NQLL.js
+++ WLYC_VKVY_LD_TYUB_VN_ZNZK_NQLL.js
@@ -1,5 +1,6 @@
 var VKVY_LD_TYUB_VN_ZNZK_NQLL =require('./VKVY_LD_TYUB_VN_ZNZK_NQLL');
+const vr_afoa_bqeo_rscs = require('./vr_afoa_bqeo_rscs');
 function WLYC_VKVY_LD_TYUB_VN_ZNZK_NQLL(req,res){
-    res.render('VKVY_LD_TYUB_VN_ZNZK_NQLL', {JTYP_1: VKVY_LD_TYUB_VN_ZNZK_NQLL(req.body.VDZV_1)});
+    res.render('VKVY_LD_TYUB_VN_ZNZK_NQLL', {JTYP_1: VKVY_LD_TYUB_VN_ZNZK_NQLL(vr_afoa_bqeo_rscs(req.body.VDZV_1))});
 }
 module.exports=WLYC_VKVY_LD_TYUB_VN_ZNZK_NQLL;
--- WLYC_VKVY_LD_TYUB_VN_ZNZK_NQUD.js
+++ WLYC_VKVY_LD_TYUB_VN_ZNZK_NQUD.js
@@ -1,5 +1,6 @@
 var VKVY_LD_TYUB_VN_ZNZK_NQUD =require('./VKVY_LD_TYUB_VN_ZNZK_NQUD');
+const vr_afoa_bqeo_rscs = require('./vr_afoa_bqeo_rscs');
 function WLYC_VKVY_LD_TYUB_VN_ZNZK_NQUD(req,res){
-    res.render('VKVY_LD_TYUB_VN_ZNZK_NQUD', {JTYP_1: VKVY_LD_TYUB_VN_ZNZK_NQUD(req.body.VDZV_1)});
+    res.render('VKVY_LD_TYUB_VN_ZNZK_NQUD', {JTYP_1: VKVY_LD_TYUB_VN_ZNZK_NQUD(vr_afoa_bqeo_rscs(req.body.VDZV_1))});
 }
 module.exports=WLYC_VKVY_LD_TYUB_VN_ZNZK_NQUD;
