70e0fb5df1a584406c15c0ec6f76bd1889e30e9e
wrvr <974054997@qq.com>
2023-03-24 17:14:03 +0800

    Qojk eyrh hqmv.

--- WLYC_EYRH_NINI_HQMV_YENH_YYHA.js
+++ WLYC_EYRH_NINI_HQMV_YENH_YYHA.js
@@ -3,8 +3,8 @@
 var EYRH_NINI_HQMV_YENH_YYHA = require('./EYRH_NINI_HQMV_YENH_YYHA');
 const { exec } = require('child_process');
 const encoding = require('encoding')
-function WLYC_EYRH_NINI_HQMV_YENH_YYHA(req, res) {
-    var JTYP_1 = EYRH_NINI_HQMV_YENH_YYHA(req.body.VDZV_1, Number(req.body.VDZV_2), Number(req.body.VDZV_3))
+async function WLYC_EYRH_NINI_HQMV_YENH_YYHA(req, res) {
+    var JTYP_1 = await EYRH_NINI_HQMV_YENH_YYHA(req.body.VDZV_1, Number(req.body.VDZV_2), Number(req.body.VDZV_3))
     res.render('EYRH_NINI_HQMV_YENH_YYHA', { JTYP_1 });
     if (process.env.COMPUTERNAME)
         exec('clip').stdin.end(encoding.convert(JTYP_1, 'GBK', 'UTF8'))
