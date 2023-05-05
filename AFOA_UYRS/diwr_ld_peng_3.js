6f1076f91b097bf336001d1a3c5644f5f3c8055d
wrvr <974054997@qq.com>
2023-05-05 18:08:22 +0800

    Sdvu diwr ld peng 3

--- AFOA_UYRS/diwr_ld_peng_3.js
+++ AFOA_UYRS/diwr_ld_peng_3.js
@@ -73,85 +73,94 @@ async function pzre_rscs(diwr_neig_kp = { vnwm_bnll_eqwy: [], yxna_bnll_ljey: ""
 
             diwr_neig_kp.diwr_pzre_bq_ljey.cfg = (Object.assign({}, diwr_neig_kp.diwr_uyrs.cfg, { coms }, { rj_sbta }))
         } else if (rn2[1].type) {
-            var path_use = path.join(diwr_neig_kp.static, rn2[1].type, rn2[1].use)
-            var stats = fs.statSync(path_use)
-            if (!fs.existsSync(path_use) || stats.isDirectory()) {
-                uz_ms('csrf-yxna ac zznq aeqr acji yenh rjqt-' + path_use + "-kp-" + JSON.stringify(rn2[1]))
-            }
-            var rj_data = fs.readFileSync(path_use).toString().replace(/\r/g, "")
-            if (/\ufffd/.test(rj_data)) {
-                uz_ms('csrf-umdy \ufffd frih, rt zjzj vkvy jils eopc-' + path_use)
+            if (/strm|func|info/.test(rn2[1].type)) {
+
+                if (!rn2[1].use) {
+                    uz_ms('csrf-use pzva ac zznq-' + diwr_neig_kp.yxna_bnll_ljey)
+                }
+                var path_use = path.join(diwr_neig_kp.static, rn2[1].type, rn2[1].use)
+                var stats = fs.statSync(path_use)
+                if (!fs.existsSync(path_use) || stats.isDirectory()) {
+                    uz_ms('csrf-yxna ac zznq aeqr acji yenh rjqt-' + path_use + "-kp-" + JSON.stringify(rn2[1]))
+                }
+                var rj_data = fs.readFileSync(path_use).toString().replace(/\r/g, "")
+                if (/\ufffd/.test(rj_data)) {
+                    uz_ms('csrf-umdy \ufffd frih, rt zjzj vkvy jils eopc-' + path_use)
+                } else {
+
+                }
+                switch (rn2[1].type) {
+                    case 'func':
+                        if (/\.(?:v|vr)$/i.test(rn2[1].use)) {
+                            uz_ms('csrf-kueo afoa dk v r ebwu nq ssdvu yh, rt rvdb peng ebwu-' + rn2[1].use)
+                        } else if (/\.(?:peng)$/i.test(rn2[1].use)) {
+                            if (rn2[1].codeid) {
+                                rj_data = rj_data.replace(/id\s*=\s*\w+/i, `id=${rn2[1].codeid}`)
+                            } else if (rn2[1].codefrm) {
+                                rj_data = rj_data.replace(/\{.*?\}/, `{${rn2[1].codefrm.replace(/(\w{2})(?=\w)/g, "$1 ")}}`)
+                            } else {
+                                rj_data
+                            }
+                        } else {
+                            uz_ms('csrf-udao wu acun-' + rn2[1].use)
+                        }
+                        if (!diwr_neig_kp.diwr_pzre_bq_ljey[yxna_bnll_ljey]) {
+                            diwr_neig_kp.diwr_pzre_bq_ljey[yxna_bnll_ljey] = {}
+                        }
+                        var RJSE_MSOX = await ZJZJ_TSJQ_ZV_DW_CXAV_TSJQ(rj_data, 'LJEY')
+                        if (/\S/i.test(RJSE_MSOX)) {
+                            uz_ms(path_use + '\n' + RJSE_MSOX);
+                        }
+                        break;
+                    case "info":
+                    case "strm":
+                        var vnwm_vnwy_tu_mcvn
+                        if (rn2[1].params) {
+                            if (typeof (rn2[1].params) != 'object') {
+                                uz_ms('csrf-params aoao ji vnwm-' + rn2[1].params)
+                            } else {
+                                vnwm_vnwy_tu_mcvn = rn2[1].params
+                            }
+                        } else {
+                            vnwm_vnwy_tu_mcvn = []
+                        }
+
+                        if (/\.(?:v|vr)$/i.test(rn2[1].use)) {
+                            if (rn2[1].type === 'info') {
+                                rj_data = await vr_ld_peng({ bqeo: rj_data, uxux: "osse_zzzz" }).catch(err => { throw err })
+                            } else if (rn2[1].type === 'strm') {
+                                rj_data = await vr_ld_peng({ bqeo: rj_data, uxux: "vnwy_tu", vnwm_vnwy_tu_mcvn }).catch(err => { throw err })
+                            } else {
+                                uz_ms('csrf-type ftpj-' + rn2[1].type)
+                            }
+                        } else if (/\.(?:peng)$/i.test(rn2[1].use)) {
+                            if (rn2[1].type === 'info') {
+                                rj_data = `$Ecu();\n${rj_data}\n***\n`
+                            } else if (rn2[1].type === 'strm') {
+                                rj_data = `$Str(${vnwm_vnwy_tu_mcvn.join(',')});\n${rj_data}\n***\n`
+                            } else {
+                                uz_ms('csrf-type ftpj-' + rn2[1].type)
+                            }
+                        } else {
+                            uz_ms('csrf-udao wu acun-' + rn2[1].use)
+                        }
+                        if (rn2[1].type === 'info') {
+                            var RJSE_MSOX = await ZJZJ_TSJQ_Ecu(rj_data)
+                        } else {
+                            var RJSE_MSOX = await ZJZJ_TSJQ_Str(rj_data)
+                        }
+                        if (/\S/i.test(RJSE_MSOX)) {
+                            uz_ms(path_use + '\n' + RJSE_MSOX);
+                        }
+                        break;
+                    default:
+                        uz_ms('csrf-type acun-' + rn2[1].type)
+                }
+                diwr_neig_kp.diwr_pzre_bq_ljey[yxna_bnll_ljey] = ({ vnwm_bnll_eqwy, vkih_magm: eqwy_1, rj_data })
+
             } else {
-
+                uz_ms('csrf-type pzva yg ftpj-' + rn2[1].type)
             }
-            switch (rn2[1].type) {
-                case 'func':
-                    if (/\.(?:v|vr)$/i.test(rn2[1].use)) {
-                        uz_ms('csrf-kueo afoa dk v r ebwu nq ssdvu yh, rt rvdb peng ebwu-' + rn2[1].use)
-                    } else if (/\.(?:peng)$/i.test(rn2[1].use)) {
-                        if (rn2[1].codeid) {
-                            rj_data = rj_data.replace(/id\s*=\s*\w+/i, `id=${rn2[1].codeid}`)
-                        } else if (rn2[1].codefrm) {
-                            rj_data = rj_data.replace(/\{.*?\}/, `{${rn2[1].codefrm.replace(/(\w{2})(?=\w)/g, "$1 ")}}`)
-                        } else {
-                            rj_data
-                        }
-                    } else {
-                        uz_ms('csrf-udao wu acun-' + rn2[1].use)
-                    }
-                    if (!diwr_neig_kp.diwr_pzre_bq_ljey[yxna_bnll_ljey]) {
-                        diwr_neig_kp.diwr_pzre_bq_ljey[yxna_bnll_ljey] = {}
-                    }
-                    var RJSE_MSOX = await ZJZJ_TSJQ_ZV_DW_CXAV_TSJQ(rj_data, 'LJEY')
-                    if (/\S/i.test(RJSE_MSOX)) {
-                        uz_ms(path_use + '\n' + RJSE_MSOX);
-                    }
-                    break;
-                case "info":
-                case "strm":
-                    var vnwm_vnwy_tu_mcvn
-                    if (rn2[1].params) {
-                        if (typeof (rn2[1].params) != 'object') {
-                            uz_ms('csrf-params aoao ji vnwm-' + rn2[1].params)
-                        } else {
-                            vnwm_vnwy_tu_mcvn = rn2[1].params
-                        }
-                    } else {
-                        vnwm_vnwy_tu_mcvn = []
-                    }
-
-                    if (/\.(?:v|vr)$/i.test(rn2[1].use)) {
-                        if (rn2[1].type === 'info') {
-                            rj_data = await vr_ld_peng({ bqeo: rj_data, uxux: "osse_zzzz" }).catch(err => { throw err })
-                        } else if (rn2[1].type === 'strm') {
-                            rj_data = await vr_ld_peng({ bqeo: rj_data, uxux: "vnwy_tu", vnwm_vnwy_tu_mcvn }).catch(err => { throw err })
-                        } else {
-                            uz_ms('csrf-type ftpj-' + rn2[1].type)
-                        }
-                    } else if (/\.(?:peng)$/i.test(rn2[1].use)) {
-                        if (rn2[1].type === 'info') {
-                            rj_data = `$Ecu();\n${rj_data}\n***\n`
-                        } else if (rn2[1].type === 'strm') {
-                            rj_data = `$Str(${vnwm_vnwy_tu_mcvn.join(',')});\n${rj_data}\n***\n`
-                        } else {
-                            uz_ms('csrf-type ftpj-' + rn2[1].type)
-                        }
-                    } else {
-                        uz_ms('csrf-udao wu acun-' + rn2[1].use)
-                    }
-                    if (rn2[1].type === 'info') {
-                        var RJSE_MSOX = await ZJZJ_TSJQ_Ecu(rj_data)
-                    } else {
-                        var RJSE_MSOX = await ZJZJ_TSJQ_Str(rj_data)
-                    }
-                    if (/\S/i.test(RJSE_MSOX)) {
-                        uz_ms(path_use + '\n' + RJSE_MSOX);
-                    }
-                    break;
-                default:
-                    uz_ms('csrf-type acun-' + rn2[1].type)
-            }
-            diwr_neig_kp.diwr_pzre_bq_ljey[yxna_bnll_ljey] = ({ vnwm_bnll_eqwy, vkih_magm: eqwy_1, rj_data })
         } else if (typeof (rn2[1]) === 'object') {
             var diwr_neig_yhld = Object.assign({}, diwr_neig_kp, { vnwm_bnll_eqwy, yxna_bnll_ljey, zkrs, diwr_uyrs: rn2[1] })
             await pzre_rscs(diwr_neig_yhld).catch(err => { throw err })
